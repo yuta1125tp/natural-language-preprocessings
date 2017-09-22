909a793d80d3f3d9ff9d299b2f27b77a2eec22ec
gasyoh <gasyoh@gmail.com>
2017-09-22 18:24:16 +0900

    Add normalize_neologd
    https://github.com/neologd/mecab-ipadic-neologd/wiki/Regexp

--- preprocessings/ja/normalization.py
+++ preprocessings/ja/normalization.py
@@ -42,3 +42,58 @@ def normalize_number(text):
     # 連続した数字を0で置換
     replaced_text = re.sub(r'\d+', '0', text)
     return replaced_text
+
+
+# refer to https://github.com/neologd/mecab-ipadic-neologd/wiki/Regexp
+def unicode_normalize(cls, text):
+    pt = re.compile('([{}]+)'.format(cls))
+
+    def norm(c):
+        return unicodedata.normalize('NFKC', c) if pt.match(c) else c
+
+    text = ''.join(norm(x) for x in re.split(pt, text))
+    text = re.sub('－', '-', text)
+    return text
+
+
+def remove_extra_spaces(text):
+    text = re.sub('[ 　]+', ' ', text)
+    blocks = ''.join(('\u4E00-\u9FFF',  # CJK UNIFIED IDEOGRAPHS
+                      '\u3040-\u309F',  # HIRAGANA
+                      '\u30A0-\u30FF',  # KATAKANA
+                      '\u3000-\u303F',  # CJK SYMBOLS AND PUNCTUATION
+                      '\uFF00-\uFFEF'  # HALFWIDTH AND FULLWIDTH FORMS
+                      ))
+    basic_latin = '\u0000-\u007F'
+
+    def remove_space_between(cls1, cls2, s):
+        p = re.compile('([{}]) ([{}])'.format(cls1, cls2))
+        while p.search(s):
+            s = p.sub(r'\1\2', s)
+        return s
+
+    text = remove_space_between(blocks, blocks, text)
+    text = remove_space_between(blocks, basic_latin, text)
+    text = remove_space_between(basic_latin, blocks, text)
+    return text
+
+
+def normalize_neologd(text):
+    text = text.strip()
+    text = unicode_normalize('０-９Ａ-Ｚａ-ｚ｡-ﾟ', text)
+
+    def maketrans(f, t):
+        return {ord(x): ord(y) for x, y in zip(f, t)}
+
+    text = re.sub('[˗֊‐‑‒–⁃⁻₋−]+', '-', text)  # normalize hyphens
+    text = re.sub('[﹣－ｰ—―─━ー]+', 'ー', text)  # normalize choonpus
+    text = re.sub('[~∼∾〜〰～]', '', text)  # remove tildes
+    text = text.translate(
+        maketrans('!"#$%&\'()*+,-./:;<=>?@[¥]^_`{|}~｡､･｢｣',
+                  '！”＃＄％＆’（）＊＋，－．／：；＜＝＞？＠［￥］＾＿｀｛｜｝〜。、・「」'))
+
+    text = remove_extra_spaces(text)
+    text = unicode_normalize('！”＃＄％＆’（）＊＋，－．／：；＜＞？＠［￥］＾＿｀｛｜｝〜', text)  # keep ＝,・,「,」
+    text = re.sub('[’]', '\'', text)
+    text = re.sub('[”]', '"', text)
+    return text
